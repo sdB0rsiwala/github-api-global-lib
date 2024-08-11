5bef8ed4fc3955cded97f410bb71382e60db9ea3
sdB0rsiwala <saurabhborsiwala.officialwork@gmail.com>
2024-08-11 16:40:34 -0400

    changed createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -1,23 +1,24 @@
 import groovy.json.JsonSlurper
 
 def withCurl(String jsonParameters) {
+    script {
+        def jsonSlurper = new JsonSlurper()
+        def apiParams = jsonSlurper.parseText(jsonParameters)
 
-    def jsonSlurper = new JsonSlurper()
-    def apiParams = jsonSlurper.parseText(jsonParameters)
+        def payload = """{
+            "name": "${apiParams.name}",
+            "description": "${apiParams.des}",
+            "private": ${apiParams.private}
+        }"""
+        sh '''
+            curl -L \\
+                -X POST \\
+                -H "Accept: application/vnd.github+json" \\
+                -H "Authorization: Bearer ${apiParams.token}" \\
+                -H "X-GitHub-Api-Version: 2022-11-28" \\
+                https://api.github.com/user/repos \\
+                -d '${payload}'
 
-    def payload = """{
-        "name": "${apiParams.name}",
-        "description": "${apiParams.des}",
-        "private": ${apiParams.private}
-    }"""
-    sh '''
-        curl -L \\
-             -X POST \\
-             -H "Accept: application/vnd.github+json" \\
-             -H "Authorization: Bearer ${apiParams.token}" \\
-             -H "X-GitHub-Api-Version: 2022-11-28" \\
-             https://api.github.com/user/repos \\
-             -d '${payload}'
-
-    '''
+        '''
+    }
 }
