04cfba2905c79f724134139b097a895f93251b0a
sdB0rsiwala <saurabhborsiwala.officialwork@gmail.com>
2024-08-11 17:08:47 -0400

    changed createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -18,7 +18,7 @@ def withCurl(String jsonParameters) {
         curl -L \\
              -X POST \\
              -H "Accept: application/vnd.github+json" \\
-             -H "Authorization: Bearer ${apiParams.token}" \\
+             -H "Authorization: Bearer %apiParams.token%" \\
              -H "X-GitHub-Api-Version: 2022-11-28" \\
              https://api.github.com/user/repos \\
              -d '${escapedPayload}'
