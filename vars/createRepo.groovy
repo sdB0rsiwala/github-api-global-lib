5a26d9dfd80237ab4fe68b70192ba511322acdad
sdB0rsiwala <saurabhborsiwala.officialwork@gmail.com>
2024-08-11 16:10:57 -0400

    created createRepo.groovy

---
+++ vars/createRepo.groovy
@@ -0,0 +1,23 @@
+import groovy.json.JsonSlurper
+
+def withCurl(string jsonParameters) {
+
+    def jsonSlurper = new JsonSlurper()
+    def apiParams = jsonSlurper.parseText(jsonParameters)
+
+    def payload = """{
+        "name": "${apiParams.name}",
+        "description": "${apiParams.des}",
+        "private": ${apiParams.private}
+    }"""
+    sh '''
+        curl -L \\
+             -X POST \\
+             -H "Accept: application/vnd.github+json" \\
+             -H "Authorization: Bearer ${apiParams.token}" \\
+             -H "X-GitHub-Api-Version: 2022-11-28" \\
+             https://api.github.com/user/repos \\
+             -d '${payload}'
+
+    '''
+}
