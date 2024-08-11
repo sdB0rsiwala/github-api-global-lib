c65985068951f1dc97a5ea0bbd4d0be7b8628819
sdB0rsiwala <saurabhborsiwala.officialwork@gmail.com>
2024-08-11 17:03:18 -0400

    changed createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -2,21 +2,26 @@ import groovy.json.JsonSlurper
 
 def withCurl(String jsonParameters) {
 
-    sh """
-        def apiParams = new groovy.json.JsonSlurper().parseText(jsonParameters)
+    def jsonSlurper = new JsonSlurper()
+    def apiParams = jsonSlurper.parseText(jsonParameters)
 
-        def payload = """{
-            "name": "${apiParams.name}",
-            "description": "${apiParams.des}",
-            "private": ${apiParams.private}
-        }"""
+    def payload = """{
+        "name": "${apiParams.name}",
+        "description": "${apiParams.des}",
+        "private": ${apiParams.private}
+    }"""
+
+    // Escape single quotes in payload for the shell
+    def escapedPayload = payload.replaceAll("'", "\\\\'")
+
+    sh '''
         curl -L \\
              -X POST \\
              -H "Accept: application/vnd.github+json" \\
              -H "Authorization: Bearer ${apiParams.token}" \\
              -H "X-GitHub-Api-Version: 2022-11-28" \\
              https://api.github.com/user/repos \\
-             -d '${payload}'
+             -d '${escapedPayload}'
 
-    """
+    '''
 }
